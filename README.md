4dafa8ec564818b6384f62aa2da2cd6220a022f8
Bree Thomas <bree@aha-method.com>
2013-11-25 14:17:57 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+[Contact-Manager](http://tutorials.jumpstartlab.com/projects/contact_manager.html)
+
+In this advanced Rails project, you’ll create a contact manager. The
+tools that you will use include the following:
+
++ Testing with RSpec to drive your development
++ Creating view templates with Haml and Sass
++ Building complex forms with Simple Form
++ Building Rails3/AREL queries and scopes
++ Handling file attachments with Carrierwave
++ Building reusable view code with helpers and partials
++ Refactoring
++ Managing authentication and authorization
++ Server and client-side validations
++ Deployment and monitoring
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ContactManager::Application.config.secret_key_base = 'a3052dd5ffb0f1b7e590da55a7bda66d1bdee8a4e5adb90aaaa041ab109b009cfcfaee5493ee93e9c0d078ea56cd8696b5fca93783bd76861d32ee277461eff5'
