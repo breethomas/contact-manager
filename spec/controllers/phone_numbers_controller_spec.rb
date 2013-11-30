1f571f0b0451b790b6a40c064ad245bef2250eb8
Bree Thomas <bree@aha-method.com>
2013-11-30 15:08:43 -0700

    Fix edit phone number workflow

--- spec/controllers/phone_numbers_controller_spec.rb
+++ spec/controllers/phone_numbers_controller_spec.rb
@@ -88,12 +88,11 @@ describe PhoneNumbersController do
 
   describe "PUT update" do
     describe "with valid params" do
+      let(:bob) { Person.create(first_name: 'Bob', last_name: 'Jones') }
+      let(:valid_attributes) { {number: '555-5678', person_id: bob.id} }
+
       it "updates the requested phone_number" do
         phone_number = PhoneNumber.create! valid_attributes
-        # Assuming there are no other phone_numbers in the database, this
-        # specifies that the PhoneNumber created on the previous line
-        # receives the :update_attributes message with whatever params are
-        # submitted in the request.
         PhoneNumber.any_instance.should_receive(:update).with({ "number" => "MyString" })
         put :update, {:id => phone_number.to_param, :phone_number => { "number" => "MyString" }}, valid_session
       end
@@ -105,9 +104,11 @@ describe PhoneNumbersController do
       end
 
       it "redirects to the phone_number" do
+        bob = Person.create(first_name: 'Bob', last_name: 'Jones')
+        valid_attributes = {number: '555-5678', person_id: bob.id}
         phone_number = PhoneNumber.create! valid_attributes
         put :update, {:id => phone_number.to_param, :phone_number => valid_attributes}, valid_session
-        response.should redirect_to(phone_number)
+        expect(response).to redirect_to(bob)
       end
     end
 
